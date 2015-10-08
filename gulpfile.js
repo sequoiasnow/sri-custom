b51425115ad344703062564c833b8649d5ba64f4
Sequoia Snow <the1codemaster@gmail.com>
2015-10-08 16:43:11 -0700

    Changed node_modules;

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp       = require( 'gulp' );
 var reactify   = require( 'reactify' );
 var rename     = require( 'gulp-rename' );
 var browserify = require( 'gulp-browserify' );
-var compass    = require( 'compass' )
+var webserver  = require( 'gulp-webserver' );
+var compass    = require( 'gulp-compass' );
 
 gulp.task( 'js', function() {
     gulp.src( 'src/main.js' )
@@ -28,6 +29,16 @@ gulp.task( 'watch', function() {
     gulp.watch( 'src/**/**/*.js', [ 'js' ] );
 
     gulp.watch( 'src/sass/**/*.scss', [ 'sass' ] );
+});
+
+gulp.task( 'server', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
 });
 
 gulp.task( 'default', [ 'js' ] );
